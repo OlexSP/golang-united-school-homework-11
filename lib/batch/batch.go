6968c0b4f308efa548f59db6feeb75045b186458
Alex Al <alexsputnik@gmail.com>
2022-07-12 18:18:10 +0300

    + mutex

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -16,16 +16,19 @@ func getOne(id int64) user {
 
 // according to the Semaphore concurrensy pattern lecture slide
 func getBatch(n int64, pool int64) (res []user) {
+	var mu sync.Mutex
 	res = make([]user, 0, int(n))
 	var wg sync.WaitGroup
 	sem := make(chan struct{}, int(pool))
 	for i := 0; i < int(n); i++ {
 		wg.Add(1)
-		sem <- struct{}{} // in the go func in the video (wrong). less running goroutines
+		sem <- struct{}{} // inside the go func in the video (wrong). less running goroutines
 		go func(i int) {  // it passes tests in bought position.
 			defer wg.Done()
 			user := getOne(int64(i))
+			mu.Lock()
 			res = append(res, user)
+			mu.Unlock()
 			<-sem
 		}(i)
 	}
