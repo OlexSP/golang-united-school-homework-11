a898cd2b96a90d00aba6c11e35b1dd140c1c9104
Alex Al <alexsputnik@gmail.com>
2022-07-12 17:48:36 +0300

    the first commit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -13,6 +14,22 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+// according to the Semaphore concurrensy pattern lecture slide
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, int(n))
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, int(pool))
+	for i := 0; i < int(n); i++ {
+		wg.Add(1)
+		sem <- struct{}{} // in the go func in the video (wrong). less running goroutines
+		go func(i int) {  // it passes tests in bought position.
+			defer wg.Done()
+			user := getOne(int64(i))
+			res = append(res, user)
+			<-sem
+		}(i)
+	}
+	wg.Wait()
+
+	return res
 }
